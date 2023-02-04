69157ae6c6cd772f546efe5ef0aa906b2601cfd0
pigluz <franciszeklatawiec@gmail.com>
2023-02-04 23:11:48 +0100

    hotfix and translate to english

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
+// Pobieranie wartości z formularza
 var imie = document.getElementById("imie");
 var nazw = document.getElementById("nazw");
 var wiek = document.getElementById("wiek");
 var mail = document.getElementById("mail");
 var pass = document.getElementById("pass");
 var psel = document.getElementById("psel");
-const selected = [];
 
+// Nasłuchiwanie kliknięcia przycisku "submit"
 document.getElementById("submit").addEventListener("click", function (e) {
-    var plec = document.querySelector('input[name="plec"]:checked');
-    var plecValue = plec ? plec.value : "";
-
-    const rodo = document.querySelector('#rodo');
-    var rodoMessage = rodo.checked ? "Zgoda na przetwarzanie danych osobowych została wyrażona." : "Zgoda na przetwarzanie danych osobowych nie została wyrażona.";
-    
-    console.log(
-        " Imie: "                   +   imie.value + "\n\n",
-        "Nazwisko: "                +   nazw.value + "\n\n",
-        "Wiek: "                    +   wiek.value + "\n\n",
-        "E-mail: "                  +   mail.value + "\n\n",
-        "Hasło: "                   +   pass.value + "\n\n",
-        "Pesel: "                   +   psel.value + "\n\n",
-        "Wybrana płeć: "            +   plecValue + "\n\n",
-        "Wybrane zainteresowania: " +   selected.join(", ") + "\n\n",
-        "Status rodo: "             +   rodoMessage + "\n\n"
-    );
-});
-
-const checkboxes = document.querySelectorAll('.checkbox_input');
-checkboxes.forEach(checkbox => {
-  checkbox.addEventListener('change', function() {
-    if (this.checked) {
-      selected.push(this.value);
-    } else {
-      for (var i = 0; i < selected.length; i++) {
-        if (selected[i] === this.value) {
-          selected.splice(i, 1);
-        }
-      }
+  // Pobieranie wybranej płci
+  var plec = document.querySelector('input[name="plec"]:checked');
+  var plecValue = plec ? plec.value : "";
+  
+  // Pobieranie wybranych zainteresowań
+  var zain = document.querySelectorAll('.checkbox_input');
+  var wybraneZain = [];
+  for (var i = 0; i < zain.length; i++) {
+    // Sprawdzenie, czy checkbox jest zaznaczony i jego wartość nie jest "on"
+    if (zain[i].checked && zain[i].value !== "on") {
+      // Dodawanie wybranego zainteresowania do tablicy
+      wybraneZain.push(zain[i].value);
     }
-  });
+  }
+  
+  // Pobieranie statusu checkboxa "rodo"
+  var rodo = document.getElementById("rodo");
+  var statusRodo = "Not confirmed";
+  if (rodo.checked) {
+    statusRodo = "Confirmed";
+  }
+  
+  // Wypisanie wszystkich wartości w konsoli
+  console.log(
+    " Name: " + imie.value + "\n\n",
+    "Surname: " + nazw.value + "\n\n",
+    "Age: " + wiek.value + "\n\n",
+    "E-mail: " + mail.value + "\n\n",
+    "Password: " + pass.value + "\n\n",
+    "SSN: " + psel.value + "\n\n",
+    "Selected gender: " + plecValue + "\n\n",
+    // Łączenie wybranych zainteresowań w jedną wartość za pomocą join()
+    "Selected interests: " + wybraneZain.join(", ") + "\n\n",
+    "Status of rodo: " + statusRodo + "\n\n")
 });
